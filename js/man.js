8d139331b5d4a24cae4756c9b187b865db71cb6e
FahrijeGjokiqi <fahrijegjokiqi1@gmail.com>
2023-06-11 20:59:00 +0200

    .

--- js/man.js
+++ js/man.js
@@ -3,15 +3,6 @@ const nav = document.getElementById('nav')
 
 toggle.addEventListener('click', () => nav.classList.toggle('links'))
 
-// const search = document.querySelector('.search')
-// const btn = document.querySelector('.btn')
-// const input = document.querySelector('.input')
-
-// btn.addEventListener('click', () => {
-//     search.classList.toggle('active')
-//     input.focus()
-// })
-
 // Function to scroll to the top of the page
 function scrollToTop() {
     window.scrollTo({
